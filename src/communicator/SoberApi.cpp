b4c9ac1d113dfaa08915776368365561ecf5cf14
Joas329 <joaquinphilco@hotmail.com>
2025-12-10 01:57:37 -0500

    SoberApi: Make responses compatible with chrome.
    Signed-off-by: Joas329 <joaquinphilco@hotmail.com>

--- src/communicator/SoberApi.cpp
+++ src/communicator/SoberApi.cpp
@@ -13,32 +13,59 @@ SoberApi::SoberApi(SystemController& controller)
 
 void SoberApi::registerRoutes(crow::SimpleApp& app)
 {
+    // Test endpoint
     CROW_ROUTE(app, "/hello")([] {
-        return "Hello from API!";
+        crow::response r("Hello from API!");
+        r.code = 200;
+        r.add_header("Content-Type", "text/plain");
+        r.add_header("Access-Control-Allow-Origin", "*");
+        return r;
     });
 
     // Turn ON optical camera
     CROW_ROUTE(app, "/camera/opt/on").methods("POST"_method)
     ([this] {
-        controller_.startOpticalCamera();
-        return crow::response(200, "Optical camera ON");
+        // Run camera start in background to avoid blocking UI
+        std::thread([this] { controller_.startOpticalCamera(); }).detach();
+
+        crow::response r("Optical camera ON");
+        r.code = 200;
+        r.add_header("Content-Type", "text/plain");
+        r.add_header("Access-Control-Allow-Origin", "*");
+        return r;
     });
 
     // Turn OFF optical camera
     CROW_ROUTE(app, "/camera/opt/off").methods("POST"_method)
     ([this] {
-        controller_.stopOpticalCamera();
-        return crow::response(200, "Optical camera OFF");
+        std::thread([this] { controller_.stopOpticalCamera(); }).detach();
+
+        crow::response r("Optical camera OFF");
+        r.code = 200;
+        r.add_header("Content-Type", "text/plain");
+        r.add_header("Access-Control-Allow-Origin", "*");
+        return r;
     });
+
+    // Capture optical (placeholder)
     CROW_ROUTE(app, "/camera/opt/capture").methods("POST"_method)
     ([] {
-        return crow::response(200, "Optical image captured");
+        crow::response r("Optical image captured");
+        r.code = 200;
+        r.add_header("Content-Type", "text/plain");
+        r.add_header("Access-Control-Allow-Origin", "*");
+        return r;
     });
 
+    // Communication test
     CROW_ROUTE(app, "/communication/test").methods("POST"_method)
     ([] {
-        return crow::response("API ONLINE");
+        crow::response r("API ONLINE");
+        r.code = 200;
+        r.add_header("Content-Type", "text/plain");
+        r.add_header("Access-Control-Allow-Origin", "*");
+        return r;
     });
 }
 
-}
+} // namespace sober::communicator
